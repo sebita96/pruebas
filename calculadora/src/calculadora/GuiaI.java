c7a15c57daf46437d0a9cb1efba01a739d0338d7
alumno <alumno>
2017-08-24 17:59:16 -0300

    primeros ejercicios

---
+++ calculadora/src/calculadora/GuiaI.java
@@ -0,0 +1,79 @@
+
+package calculadora;
+import java.util.Scanner;
+public class GuiaI {
+	public static void ejercicio1(){
+		
+		int edad;
+		
+		System.out.println("ingrese su edad");
+		
+		Scanner s = new Scanner(System.in);	
+	
+		edad=Integer.parseInt(s.nextLine());
+		
+
+		if (edad >= 18){
+			System.out.println("es mayor de edad");
+		}
+		else {
+			 System.out.println("es menor de edad");
+		}
+				
+}
+
+
+				// ejercicio6
+
+ 
+  public static void ejercicio4(){
+	  
+	  int edad ;
+	  
+	  System.out.println("ingrse su edad");
+	  
+	  Scanner s = new Scanner(System.in);
+	  
+	  edad=Integer.parseInt(s.nextLine());
+	  
+	  if ( edad>=13 && edad<=17){
+		  
+		  System.out.println("es adolesente" );
+	  }
+	  else {
+		  System.out.println("no es adolesente ");
+	  }
+  
+  }
+
+				//ejercicio54
+  
+  //jksbajhgfiuebbcfbsuibdcbaoi
+
+public static void ejercicio9(){
+	  
+	  int edad ;
+	  
+	  System.out.println("ingrese edad.");
+	  
+	  System.out.println("estado civil");
+	  
+	  Scanner s = new Scanner(System.in);
+	  
+	  edad=Integer.parseInt(s.nextLine());
+	  
+	  if (edad>=18 ){
+		  
+		  System.out.println("");
+	  }
+	  else if ( edad<=13 ){
+		    System.out.println("Es soltero y no es menor");
+	  }
+	  else{
+		  
+	  }
+	  
+	  
+
+
+
---
+++ calculadora/src/calculadora/main.java
@@ -0,0 +1,14 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//GuiaI.ejercicio4();
+		//GuiaI.ejercicio1();
+		//GuiaI.ejercicio9();
+	
+
